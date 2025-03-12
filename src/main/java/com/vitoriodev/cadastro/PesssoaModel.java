25725d58041321059e5c6f446202c4185204c3c9
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2025-03-12 16:52:13 -0300

    entidade da classe pessoa para BD

---
+++ src/main/java/com/vitoriodev/cadastro/PesssoaModel.java
@@ -0,0 +1,31 @@
+package com.vitoriodev.cadastro;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// JPA = Java Persistence API
+
+// Entity ele faz com que a classe seja uma entidade do banco de dados
+@Entity
+@Table(name = "tb_cadastro_de_pessoas")
+public class PesssoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento, ele vai passa uma estrateira chave para cada pessoa.
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public PesssoaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    
+
+}

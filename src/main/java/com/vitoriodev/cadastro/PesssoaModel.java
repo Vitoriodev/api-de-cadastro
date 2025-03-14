8ccf52285912c4ac59223c7705988d2fe148cfe7
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2025-03-13 22:01:22 -0300

    ajuste

--- src/main/java/com/vitoriodev/cadastro/PesssoaModel.java
+++ src/main/java/com/vitoriodev/cadastro/PesssoaModel.java
@@ -15,14 +15,38 @@ public class PesssoaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento, ele vai passa uma estrateira chave para cada pessoa.
-    long id;
-    String nome;
-    String email;
-    int idade;
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
 
     public PesssoaModel(String nome, String email, int idade) {
         this.nome = nome;
         this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
         this.idade = idade;
     }
 
